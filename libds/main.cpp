bf38fb6d2384e2455d6a92a918ec1c4473d5f0bb
vtta <vtta0124@gmail.com>
2018-11-28 15:53:24 +0800

    Initial Commit

---
+++ libds/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libds
+//
+//  Created by v tta on 11/28/18.
+//  Copyright © 2018 vtta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
