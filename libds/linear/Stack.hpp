171cff46c3ed71571f320b2ccc77f8ba1d2c76a5
vtta <vtta0124@gmail.com>
2018-12-22 03:25:45 +0800

    initial version of Stack implementation using SingleList

--- libds/linear/Stack.hpp
+++ libds/linear/Stack.hpp
@@ -9,6 +9,26 @@
 #ifndef Stack_hpp
 #define Stack_hpp
 
-#include <stdio.h>
+#include "../linear/SingleList.hpp"
+
+namespace libds {
+// begining of namespace libds
+
+template <typename value_type>
+class Stack : public SingleList<value_type> {
+    using node_type       = SingleListNode<value_type>;
+    using node_ptr        = node_type *;
+    using value_const_ref = value_type const &;
+    using _base           = SingleList<value_type>;
+
+public:
+    value_type top() { return _base::front()->data; }
+
+    value_type pop() { return _base::erase(_base::front()); }
+    void push(value_const_ref e) { _base::push_front(e); }
+};
+
+// end of namespace libds
+}
 
 #endif /* Stack_hpp */
