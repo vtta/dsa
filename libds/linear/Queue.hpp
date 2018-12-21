39b4fdfd499b64efe6abdd753ff02b3b0acc2006
vtta <vtta0124@gmail.com>
2018-12-22 03:27:41 +0800

    initial version of Queue implementation using SingleList

--- libds/linear/Queue.hpp
+++ libds/linear/Queue.hpp
@@ -9,6 +9,27 @@
 #ifndef Queue_hpp
 #define Queue_hpp
 
-#include <stdio.h>
+#include "../linear/SingleList.hpp"
 
+namespace libds {
+// begining of namespace libds
+
+template <typename value_type>
+class Queue : public SingleList<value_type> {
+    using node_type       = SingleListNode<value_type>;
+    using node_ptr        = node_type *;
+    using value_const_ref = value_type const &;
+    using _base           = SingleList<value_type>;
+
+public:
+    value_type pop() { return _base::erase(_base::front()); }
+    void push(value_const_ref e) { _base::push_back(e); }
+
+    value_type top() { return _base::front()->data; }
+    value_type front() { return _base::front()->data; }
+    value_type back() { return _base::back()->data; }
+};
+
+// end of namespace libds
+}
 #endif /* Queue_hpp */
